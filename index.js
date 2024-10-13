bf244aa2ce90d20564c1a7d358a46b71552dccc7
peemant <space.time.traveller@gmail.com>
2024-10-13 10:23:36 -0400

    Initial Commit
    This is the Commit with the fixes that should make it available

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import moment from "moment";
 const app = express();
 
 
-const port = 80;
+const port = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +22,8 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
 
-app.listen(port, () => {
-    console.log(`Listenning on port : ${port}`);
-});
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
